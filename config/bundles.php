f68ec34183f819e9edf9c2ba06ec5b565b2c84a0
Adrian <adrianbuzatu86@gmail.com>
2019-09-04 08:08:31 +0000

    work

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,7 @@
 <?php
 
-use Symfony\Bundle\WebServerBundle\WebServerBundle;
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
